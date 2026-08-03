dbab5bb0fc05935798d850aa8c89c77aa3f717f7
Hoàng Hùng Anh <codedr123456789@gmail.com>
2026-08-03 09:22:21 +0700

    Reply note function logically edited

--- app/Http/Controllers/reply_note.php
+++ app/Http/Controllers/reply_note.php
@@ -6,14 +6,29 @@ use Illuminate\Http\Request;
 
 class reply_note extends Controller
 {
-    public function reply_note(Request $request, $noteid, $noterepliedid){
-        $note = Note::find($noteid);
-        $replied_note = Note::find($noterepliedid);
-        if($note && $replied_note){
+    public function reply_note(Request $request, $noteid){
+        $replied_note = Note::find($noteid);
+        //$replied_note = Note::find($noterepliedid);    
+        if($replied_note){
+            $data = $request->all();
+            $note = new Note();
+            $note->title = $data['title'];
+            $note->description = $data['description'];
+            $note->creater_id = Auth::user()->id;
+            $note->reply_note_id = $replied_note->id;
+            $note->save();
+
+            $mark_as_done = new Mark_as_done();
+            $mark_as_done->noteID = $note->id;
+            $mark_as_done->userID = Auth::user()->id;
+            $mark_as_done->status = false;
+            $mark_as_done->save();
+            /*
             $note2 = Note::create([
                 "note_id"=> $noteid,
                 "replied_note_id"=> $noterepliedid,
             ]);
+            */
             return redirect('note')->with('success', 'Note replied successfully');
         }else{
             return redirect('note')->with('error', 'Note not replied');
